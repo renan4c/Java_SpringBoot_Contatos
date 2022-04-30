d74827439f38a94d53a61c90cf228d476335dd85
renan4c <renan.boxmail@gmail.com>
2022-04-30 18:19:36 -0300

    include delete and update

--- src/main/java/br/com/uniciv/contatos/ContatoController.java
+++ src/main/java/br/com/uniciv/contatos/ContatoController.java
@@ -3,9 +3,14 @@ package br.com.uniciv.contatos;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -20,7 +25,20 @@ public class ContatoController {
 	}
 	
 	@PostMapping("/contato")
+	@ResponseStatus(HttpStatus.CREATED)
 	public Contato salvar(@RequestBody Contato contato) {
+		return repository.save(contato);
+	}
+	
+	@DeleteMapping("/contato")
+	@ResponseStatus(HttpStatus.OK)
+	public void deleta(@RequestParam String id) {
+		repository.deleteById(Integer.valueOf(id));
+	}
+	
+	@PutMapping("/contato")
+	@ResponseStatus(HttpStatus.OK)
+	public Contato alterar(@RequestBody Contato contato) {
 		return repository.save(contato);
 	}
 }
